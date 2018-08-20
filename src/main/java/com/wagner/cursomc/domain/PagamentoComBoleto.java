a0e60ef00ad2a0e659d0aa1104751d399fabc9a9
Wagner Roberto <wagnerkobain@yahoo.com.br>
2018-08-20 06:14:16 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/wagner/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/wagner/cursomc/domain/PagamentoComBoleto.java
@@ -4,19 +4,24 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wagner.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
-public class PagamentoComBoleto extends Pagamento{
+public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
-	private Date dataPagamento;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataPagamento;
+
 	public PagamentoComBoleto() {
 	}
 
-	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido,Date dataVencimento, Date dataPagamento) {
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamento) {
 		super(id, estado, pedido);
 		this.dataVencimento = dataVencimento;
 		this.dataPagamento = dataPagamento;
@@ -37,8 +42,5 @@ public class PagamentoComBoleto extends Pagamento{
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
-	
-	
-	
-	
+
 }
---
+++ src/main/java/com/wagner/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.wagner.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wagner.cursomc.domain.Pedido;
+import com.wagner.cursomc.repositories.PedidoRepository;
+import com.wagner.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado! Id: "+id
+				+", Tipo: "+Pedido.class.getName()));
+	}
+}
