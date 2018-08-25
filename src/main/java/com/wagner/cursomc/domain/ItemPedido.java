3393d90ae887ad4f9cbd0287b95f1ed31795ce74
Wagner Roberto <wagnerkobain@yahoo.com.br>
2018-08-25 16:02:31 -0300

    Inserindo Pedido

--- src/main/java/com/wagner/cursomc/domain/ItemPedido.java
+++ src/main/java/com/wagner/cursomc/domain/ItemPedido.java
@@ -38,11 +38,18 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return this.id.getPedido();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		this.id.setPedido(pedido);
+	}
 
 	public Produto getProduto() {
 		return this.id.getProduto();
 	}
 
+	public void setProduto(Produto produto) {
+		this.id.setProduto(produto);
+	}
 	
 	public ItemPedidoPk getId() {
 		return id;
--- src/main/java/com/wagner/cursomc/resources/PedidoResource.java
+++ src/main/java/com/wagner/cursomc/resources/PedidoResource.java
@@ -1,13 +1,21 @@
 package com.wagner.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wagner.cursomc.domain.Categoria;
 import com.wagner.cursomc.domain.Pedido;
+import com.wagner.cursomc.dto.CategoriaDTO;
 import com.wagner.cursomc.services.PedidoService;
 
 @RestController
@@ -21,5 +29,15 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+	
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
