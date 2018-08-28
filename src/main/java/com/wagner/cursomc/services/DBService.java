efcc8231f2f9d2e30122f3d22235ee0bd7ce2ed3
Wagner Roberto <wagnerkobain@yahoo.com.br>
2018-08-27 21:49:05 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/wagner/cursomc/services/DBService.java
+++ src/main/java/com/wagner/cursomc/services/DBService.java
@@ -103,7 +103,7 @@ public class DBService {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "wagnerkobain@yahoo.com.br", "36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "382208334", cli1, c1);
